e5d6ab6f777640e11c6549cfee3bf421b338672f
ssc9309 <ssc9309@hotmail.com>
2015-05-04 18:24:03 -0400

    Init commit
    if the arduino port is open, c# can't open it. Vice Versa.
    
    Fuck.

---
+++ AutoFishFeeder/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace AutoFishFeeder
+{
+    public partial class Form1 : Form
+    {
+        //private SerialPort arduinoPort = new SerialPort("COM5", 9600);
+        private SerialPort arduinoPort = new SerialPort();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            arduinoPort.PortName = "COM5";
+            arduinoPort.BaudRate = 9600;
+
+            arduinoPort.Parity = Parity.None;
+            arduinoPort.DataBits = 8;
+            arduinoPort.StopBits = StopBits.One;
+
+            arduinoPort.DtrEnable = true;
+
+            arduinoPort.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (arduinoPort.IsOpen)
+            {
+                arduinoPort.Open();
+                arduinoPort.Write("hello");
+                arduinoPort.Close();
+            }
+            else
+            {
+                MessageBox.Show("Not open");
+            }
+        }
+    }
+}
